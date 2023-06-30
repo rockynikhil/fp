cf0620c4f827cd21ad49227e505c5d094b5459e0
nikhileswarsharma <eswarnikhil78@gmail.com>
2023-06-30 15:14:17 +0530

    first commit by rockynikhil

---
+++ filterproject/testapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.db import models
+
+# Create your models here.
+class FilterModel(models.Model):
+    name = models.CharField(max_length=30)
+    subject = models.CharField(max_length=30)
+    dept = models.CharField(max_length=30)
+    date = models.DateField()
+
